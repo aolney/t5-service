a6945afdafd1a11a95f9119c3ee869858501f57f
Andrew Olney <aolney@memphis.edu>
2021-12-02 18:19:40 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# t5-service
+
+A flask-based Docker image providing a generic pretrained T5 service.
+
+
+## Installation
+
+A conda env is provided. Simply execute:
+
+```
+conda env create -f environment.yml
+```
+followed by:
+
+```
+conda activate t5
+```
+
+## Server
+
+The server is contained in `app.py`, so all modifications should be made there.
+
+To run the server using flask, do:
+
+```
+export FLASK_APP=app.py
+flask run
+```
+
+The Dockerfile in specifies a build using [Gunicorn](https://flask.palletsprojects.com/en/1.1.x/deploying/wsgi-standalone/) for production.
+You can build a docker image with e.g.:
+
+```
+docker build --tag t5-service:1.0 .
+```
+
+and run with, e.g.:
+
+```
+docker run -p 8000:8000 t5-service:1.0
+```
+
+[Postman](https://learning.postman.com/) tests are in `t5.postman_collection.json`, with different ports for flask and gunicorn (5000 for flask, 8000 for gunicorn).
+These tests document how to call the server, but essentially it is POSTing JSON like this:
+
+```json
+{
+    "text":"Epithelial tissue, also referred to as  <extra_id_0>, refers to the sheets of cells that cover exterior surfaces of the body, line internal cavities and passageways, and form certain glands."
+}
+```
---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Service set up
+
+import torch
+from transformers import T5ForConditionalGeneration,T5Config,T5Tokenizer
+
+def set_seed(seed):
+  torch.manual_seed(seed)
+  if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+
+set_seed(42)
+
+t5_version = 't5-large'
+
+config = T5Config.from_pretrained(t5_version)
+tokenizer = T5Tokenizer.from_pretrained(t5_version)
+model = T5ForConditionalGeneration.from_pretrained(t5_version,config=config)
+
+# DEBUG
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+print ("device ",device)
+model = model.to(device)
+
+#=======================================================================================
+# Service functions
+# Fill in <extra_id_0> token
+def getFillIn(text, fillLength, responseCount):
+
+    # encode text
+    encoding = tokenizer.encode_plus(text, add_special_tokens=True, return_tensors="pt")
+    input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
+
+    # set top_k = 50 and set top_p = 0.95 and num_return_sequences = 3
+    outputs = model.generate(
+        input_ids=input_ids, attention_mask=attention_masks,
+        do_sample=True,
+        max_length=fillLength,
+        top_k=120,
+        top_p=0.98,
+        early_stopping=True,
+        num_return_sequences=responseCount
+    )
+
+    _0_index = text.index('<extra_id_0>')
+    _result_prefix = text[:_0_index]
+    _result_suffix = text[_0_index+12:]  # 12 is the length of <extra_id_0>
+
+    def _filter(output, end_token='<extra_id_1>'):
+        # The first token is <unk> (inidex at 0) and the second token is <extra_id_0> (indexed at 32099)
+        _txt = tokenizer.decode(output[2:], skip_special_tokens=False, clean_up_tokenization_spaces=False)
+        if end_token in _txt:
+            _end_token_index = _txt.index(end_token)
+            return _result_prefix + _txt[:_end_token_index] + _result_suffix
+        else:
+            return _result_prefix + _txt + _result_suffix
+
+    results = list(map(_filter, outputs))
+
+    #remove dups
+    final_outputs =[]
+    for r in results:
+        if r not in final_outputs:
+            final_outputs.append(r)
+    
+    return final_outputs
+
+#=======================================================================================
+# Instructions/help
+@app.route('/')
+def api_help():
+    return 'API for t5-service, see https://github.com/aolney/t5-service'
+
+# getFillIn(text,fillLength)
+@app.route('/api/getFillIn', methods=['GET', 'POST'])
+def api_getFillIn():
+    content = request.get_json()
+    result = getFillIn( content['text'], int(content['fillLength']), int(content['responseCount']) )
+    return jsonify(result)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST') #GET,PUT,POST,DELETE')
+    return response
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
